853ac1edcf586cbacbacee6b3ac3101a6538f245
Collin Chapman <cchap14@gmail.com>
2021-09-28 20:40:47 -0700

    add .map to map through savedBooks property

--- src/components/SavedList.js
+++ src/components/SavedList.js
@@ -3,12 +3,28 @@ import PropTypes from 'prop-types';
 
 function SavedList(props) {
   return (
-    <React.Fragment>
-      <p>saved list will go here</p>
+    <>
+    <h1>Saved Books</h1>
+      <ul>
+        {props.savedBooks.map((bookData, index) =>
+          <li key={index}>
+            <p>{bookData.volumeInfo.title}</p>
+            <p>Author(s): {bookData.volumeInfo.authors}</p>
+            <p>Genre: {bookData.volumeInfo.categories}</p>
+            <p>Average Rating: {bookData.volumeInfo.averageRating === undefined
+              ? "No Ratings Available"
+              : bookData.volumeInfo.averageRating} </p>
+          
+            <img src={bookData.volumeInfo.imageLinks === undefined
+              ? "https://previews.123rf.com/images/pavelstasevich/pavelstasevich1811/pavelstasevich181101065/112815953-no-image-available-icon-flat-vector.jpg"
+              : bookData.volumeInfo.imageLinks.thumbnail} alt="book cover thumbnail"/>
 
-      <h1>Saved Books</h1>
-
-    </React.Fragment>
+            {/* <button onClick= { () => handleSavingBook(bookData) }>Add To Readlist</button> */}
+            { console.log(bookData.id) }
+          </li>
+        )}
+      </ul>
+    </>
   );
 }
 
