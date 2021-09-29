fe5b605e11fd8c2342fd56935f2e46fdbb25cee9
Collin Chapman <cchap14@gmail.com>
2021-09-28 21:07:14 -0700

    move handleClickSaved function from BookControl up to App and passing savedBooks array from App to SavedList component via props and hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import BookControl from "./BookControl";
 import SavedList from "./SavedList";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
+
+  const [savedBooks, setSavedBooks] = useState([]);
+  //useState is a hook that declares savedBooks as our state variable
+  //useState has an argument of our initial state, in this case it is a blank array
+  //useState takes the initial value of savedBooks. equivalent to state constructor in class component
+  //setSavedBooks is a function that updates our current state (savedBooks). equivalent to setState that gets used in class component
+  //App component will re-render whenever setSavedBooks is called
+
+
+  useEffect(() => {
+    //the function inside here will be called on at the end of each lifecycle (this is where i will get collection from firestore) and then call setSavedBooks and pass in the result (setSavedBooks will set the value of savedBooks)
+    console.log("useEffect reached");
+  }, []) 
+  // second parameter of empty array means that this useEffect function will only be used one time, when the component mounts
+
+  const handleClickSaved = (book) => {
+    console.log("handle click saved from App reached");
+    console.log(book);
+    const newSavedBooksCollection = savedBooks.concat(book);
+    setSavedBooks(newSavedBooksCollection);
+    console.log(newSavedBooksCollection);
+    //this is where i would add saved book to firestore "saved" collection
+  }
+
   return (
     <Router>
       <Header />
       <Switch>
         <Route path="/saved">
-          <SavedList />
+          <SavedList savedBooks={savedBooks} />
         </Route>
         <Route path="/">
-          <BookControl />
+          <BookControl onClickSaved={handleClickSaved} />
         </Route>
       </Switch>
     </Router>
