e57643a42160f9a759ca8ee54ab1f4e27701d630
Collin Chapman <cchap14@gmail.com>
2021-10-04 22:15:49 -0700

    add handleRemovingBook button

--- src/components/DoneReadList.js
+++ src/components/DoneReadList.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function DoneReadList(props) {
 
+  function handleRemovingBook(book) {
+    props.onClickRemoveBookFromCompleted(book);
+  }
 
   return (
     <>
@@ -16,12 +20,16 @@ function DoneReadList(props) {
               ? "https://previews.123rf.com/images/pavelstasevich/pavelstasevich1811/pavelstasevich181101065/112815953-no-image-available-icon-flat-vector.jpg"
               : bookData.volumeInfo.imageLinks.thumbnail} alt="book cover thumbnail"/>
 
-            {/* <button onClick= { () => handleSavingBook(bookData) }>Add To Readlist</button> */}
+            <button onClick= { () => handleRemovingBook(bookData.volumeInfo.title) }>Remove</button>
           </li>
         )}
       </ul>
     </>
   );
+}
+
+DoneReadList.propTypes = {
+  onClickRemoveBookFromCompleted: PropTypes.func,
 }
 
 export default DoneReadList;
