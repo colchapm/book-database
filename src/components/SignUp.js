b423c92ddc467a6edd95d1b638bf8fb9e171a6dd
Collin Chapman <cchap14@gmail.com>
2021-10-04 19:11:29 -0700

    create signup component

---
+++ src/components/SignUp.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+export default function SignUp(props) {
+  const history = useHistory();
+
+  const handleSubmit = (ev) => {
+    ev.preventDefault();
+    const form = ev.currentTarget;
+    if (form.password.value === form.password2.value) {
+      const auth = getAuth();
+
+      createUserWithEmailAndPassword(auth, form.email.value, form.password.value)
+        .then((userCredential) => {
+          // Signed in 
+          const user = userCredential.user;
+          console.log(userCredential);
+          history.push('/');
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          console.log(error);
+          // ..
+        });
+    } else {
+      alert("Passwords don't match");
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>Email: <input name='email' type='text' /></div>
+      <div>Password: <input name='password' type='password' /></div>
+      <div>Confirm Password: <input name='password2' type='password' /></div>
+      <button type='submit'>Sign Up</button>
+    </form>
+  );
+}
