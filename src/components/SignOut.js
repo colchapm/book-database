93defb62494038c54ec85c488e55aba2eeb0fd59
Collin Chapman <cchap14@gmail.com>
2021-10-04 19:57:07 -0700

    create signout component

---
+++ src/components/SignOut.js
@@ -0,0 +1,23 @@
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import { getAuth, signOut } from "firebase/auth";
+
+function SignIn(props) {
+  const history = useHistory();
+  
+  useEffect(() => {
+    const auth = getAuth();
+    signOut(auth).then(() => {
+      console.log('logout')
+      props.onSuccess();
+      history.push('/login');
+    }).catch((error) => {
+      // An error happened.
+      console.log(error);
+    });
+  })
+
+  return null;
+}
+
+export default SignIn;
