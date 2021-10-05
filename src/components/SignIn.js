d6fb982c85a656072b47e7f253355f3ec98eda50
Collin Chapman <cchap14@gmail.com>
2021-10-04 19:35:13 -0700

    create signin component

---
+++ src/components/SignIn.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+function SignIn(props) {
+  const history = useHistory();
+  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.currentTarget;
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, form.email.value, form.password.value)
+      .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        props.onSuccess(user);
+        history.push('/');
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(error);
+      });
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>Email: <input name='email' type='text' /></div>
+      <div>Password: <input name='password' type='password' /></div>
+      <button>Login</button>
+      <div><Link to='/signup'>Sign Up</Link></div>
+    </form>
+  );
+}
+
+export default SignIn;
