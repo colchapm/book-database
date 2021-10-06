c3ceee9dd91f5af73313b0183eb1c8fe3f605154
Collin Chapman <cchap14@gmail.com>
2021-10-05 22:28:37 -0700

    add anchor tags within the book cover images in DoneReadList, SavedList, SearchResultList components - on click opens googlebooks info page in new tab

--- src/components/DoneReadList.js
+++ src/components/DoneReadList.js
@@ -16,9 +16,9 @@ function DoneReadList(props) {
             <p>{bookData.volumeInfo.title}</p>
             <p>Author(s): {bookData.volumeInfo.authors}</p>
             {/* <p>Year Read</p> */}
-            <img src={bookData.volumeInfo.imageLinks === undefined
+            <a style={{display: "table-cell"}} href={bookData.volumeInfo.infoLink} target="_blank" rel="noopener noreferrer"> <img src={bookData.volumeInfo.imageLinks === undefined
               ? "https://previews.123rf.com/images/pavelstasevich/pavelstasevich1811/pavelstasevich181101065/112815953-no-image-available-icon-flat-vector.jpg"
-              : bookData.volumeInfo.imageLinks.thumbnail} alt="book cover thumbnail"/>
+              : bookData.volumeInfo.imageLinks.thumbnail} alt="book cover thumbnail"/> </a>
 
             <button className="book_button" onClick= { () => handleRemovingBook(bookData.volumeInfo.title) }>Remove</button>
           </li>
--- src/components/SavedList.js
+++ src/components/SavedList.js
@@ -25,9 +25,9 @@ function SavedList(props) {
               ? "No Ratings Available"
               : bookData.volumeInfo.averageRating} </p>
           
-            <img src={bookData.volumeInfo.imageLinks === undefined
+              <a style={{display: "table-cell"}} href={bookData.volumeInfo.infoLink} target="_blank" rel="noopener noreferrer"> <img src={bookData.volumeInfo.imageLinks === undefined
               ? "https://previews.123rf.com/images/pavelstasevich/pavelstasevich1811/pavelstasevich181101065/112815953-no-image-available-icon-flat-vector.jpg"
-              : bookData.volumeInfo.imageLinks.thumbnail} alt="book cover thumbnail"/>
+              : bookData.volumeInfo.imageLinks.thumbnail} alt="book cover thumbnail"/> </a>
 
             <button className="book_button" onClick= { () => { handleMarkingRead(bookData); handleRemovingBook(bookData.volumeInfo.title); }}>Mark As Read</button>
             <button className="book_button" onClick= { () => handleRemovingBook(bookData.volumeInfo.title) }>Remove Book</button>
