8e6c67f146a401c49cfc0a7071929a71ce1e4f00
Collin Chapman <cchap14@gmail.com>
2021-10-06 20:08:39 -0700

    add grid and style DoneReadList

--- src/components/DoneReadList.js
+++ src/components/DoneReadList.js
@@ -9,18 +9,24 @@ function DoneReadList(props) {
 
   return (
     <>
-    <h1>Read History</h1>
-      <ul>
+    <hr />
+    <p className="header_subtext">Read History</p>
+      <ul className="container">
         {props.completedBooks.map((bookData, index) =>
-          <li key={index}>
-            <p>{bookData.volumeInfo.title}</p>
-            <p>Author(s): {bookData.volumeInfo.authors}</p>
+          <li className="li_no_bullets" key={index}>
+            <p className="book_title_text">{bookData.volumeInfo.title}</p>
+            <p className="book_author_text">Author(s): {bookData.volumeInfo.authors}</p>
             {/* <p>Year Read</p> */}
-            <a style={{display: "table-cell"}} href={bookData.volumeInfo.infoLink} target="_blank" rel="noopener noreferrer"> <img src={bookData.volumeInfo.imageLinks === undefined
-              ? "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXhzNKj_YNwD8fENvC1wGMxq6_zUxI1FmLvQ&usqp=CAU"
-              : bookData.volumeInfo.imageLinks.thumbnail} alt="book cover thumbnail"/> </a>
 
-            <button className="book_button" onClick= { () => handleRemovingBook(bookData.volumeInfo.title) }>Remove</button>
+            <div className="thumbnail">
+              <a style={{display: "table-cell"}} href={bookData.volumeInfo.infoLink} target="_blank" rel="noopener noreferrer"> <img src={bookData.volumeInfo.imageLinks === undefined
+                ? "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXhzNKj_YNwD8fENvC1wGMxq6_zUxI1FmLvQ&usqp=CAU"
+                : bookData.volumeInfo.imageLinks.thumbnail} alt="book cover thumbnail"/> </a>
+              </div>
+
+            <div className="thumbnail">
+              <button className="book_button" onClick= { () => handleRemovingBook(bookData.volumeInfo.title) }>Remove</button>
+            </div>
           </li>
         )}
       </ul>
