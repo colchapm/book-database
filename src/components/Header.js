22006cbad61549402660eb00178659adad556097
Collin Chapman <cchap14@gmail.com>
2021-10-06 20:46:39 -0700

    delete comments

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,17 +7,6 @@ function Header(){
       <div className="header_page">
         <h1 className="header">bookbox</h1>
         <img className="photo" src={tempCoverPhoto} alt="book covers" />
-        {/* <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/saved">Saved</Link>
-          </li>
-          <li>
-            <Link to="/history">History</Link>
-          </li>
-        </ul> */}
         <p className="header_subtext">track books you've read.</p>
         <p className="header_subtext">save those you want to read.</p>
       </div>
