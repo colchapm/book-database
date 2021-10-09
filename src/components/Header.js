9f96671631535c3f7023fdf8173f60eb8396fdab
Collin Chapman <cchap14@gmail.com>
2021-10-09 10:34:56 -0700

    add updated cover photo and style subtext to fit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import tempCoverPhoto from "./../img/temporary_cover_photo.jpg"
+import coverPhoto from "./../img/bookboxcover.png"
 
 function Header(){
   return (
     <React.Fragment>
       <div className="header_page">
-        <h1 className="header">bookbox</h1>
-        <img className="photo" src={tempCoverPhoto} alt="book covers" />
-        <p className="header_subtext">track books you've read.</p>
-        <p className="header_subtext">save those you want to read.</p>
+        <img className="photo" src={coverPhoto} alt="book covers" />
+        <p className="header_subtext1">track books you've read.</p>
+        <p className="header_subtext2">save those you want to read.</p>
       </div>
 
     </React.Fragment>
