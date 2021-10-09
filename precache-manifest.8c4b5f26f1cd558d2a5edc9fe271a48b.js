3314e04625de8213a0344b5473799c5c1323a7c3
Collin Chapman <cchap14@gmail.com>
2021-10-09 10:45:46 -0700

    Updates

---
+++ precache-manifest.8c4b5f26f1cd558d2a5edc9fe271a48b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b4c689e9c6ff39024459a9adbaf2842",
+    "url": "/book-database/index.html"
+  },
+  {
+    "revision": "cf8f3445217b3163a6e9",
+    "url": "/book-database/static/css/main.66782137.chunk.css"
+  },
+  {
+    "revision": "e0bc6cbbfd068dcb0b49",
+    "url": "/book-database/static/js/2.ab9b9843.chunk.js"
+  },
+  {
+    "revision": "66ece08be339978ce309",
+    "url": "/book-database/static/js/3.ad8b8644.chunk.js"
+  },
+  {
+    "revision": "cf8f3445217b3163a6e9",
+    "url": "/book-database/static/js/main.d06bacf2.chunk.js"
+  },
+  {
+    "revision": "fec3ba754c0fbd536acb",
+    "url": "/book-database/static/js/runtime-main.35215ad9.js"
+  },
+  {
+    "revision": "068cb611ccedc0806600c34d7a2f36ff",
+    "url": "/book-database/static/media/bookboxcover.068cb611.png"
+  }
+]);
